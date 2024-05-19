e88f334a08068fe88586cbe3e2e22d28edeae8cd
VincentWei <vincent@minigui.org>
2024-05-19 20:26:37 +0800

    the nature addition and mulitplifier are ready by using string

--- source/cpp/noi/p1009.cpp
+++ source/cpp/noi/p1009.cpp
@@ -1,3 +1,7 @@
+/* This implementation is too slow.
+   We need to optimize it forthmore.
+*/
+
 #include <iostream>
 #include <string>
 
@@ -9,14 +13,14 @@ void natural_add_ap(string &result, const string &a, const string &b)
     size_t len_b = b.length();
     size_t len_max = (len_a > len_b) ? len_a : len_b;
 
+    result.clear();
     int c = 0;
     for (size_t i = 0; i < len_max; i++) {
         int n_a = ((i < len_a) ? a[len_a - i - 1] : '0') - '0';
         int n_b = ((i < len_b) ? b[len_b - i - 1] : '0') - '0';
 
-        int r_org = n_a + n_b;
-        int r = r_org + c;
-        if (r > 10) {
+        int r = n_a + n_b + c;
+        if (r >= 10) {
             c = 1;
             r -= 10;
         }
@@ -42,32 +46,77 @@ bool natural_not_zero(const string &n)
     return false;
 }
 
-void natural_reduce_ap(string &result, const string &a, const string &b)
-{
-}
-
 void natural_mul_ap(string &result, const string &a, const string &b)
 {
-    string summary = "0";
+    string times = "0";
     string alt_b = b;
-    while (natural_not_zero(alt_b)) {
-        string result;
-        natural_add_ap(result, summary, a);
-        summary = result;
 
-        natural_reduce_ap(result, alt_b, "1");
-        result = alt_b;
+    result = "0";
+    while (b != times) {
+        string tmp;
+        natural_add_ap(tmp, result, a);
+        result = tmp;
+
+        natural_add_ap(tmp, times, "1");
+        times = tmp;
+    }
+}
+
+void natural_factorial_ap(string &r, const string &n)
+{
+    string times = "1";
+
+    r = n;
+    while (n != times) {
+        string tmp;
+        natural_mul_ap(tmp, times, r);
+        r = tmp;
+
+        natural_add_ap(tmp, times, "1");
+        times = tmp;
+    }
+}
+
+void summary_of_factorials(string &r, const string &n)
+{
+    string times = "1";
+
+    r = "0";
+    while (true) {
+        string factorial, summary;
+        natural_factorial_ap(factorial, times);
+        natural_add_ap(summary, r, factorial);
+
+        r = summary;
+
+        if (n == times)
+            break;
+
+        string tmp;
+        natural_add_ap(tmp, times, "1");
+        times = tmp;
     }
 }
 
 int main()
 {
-    string a, b;
+    string n;
+    cin >> n;
 
+    string summary;
+    summary_of_factorials(summary, n);
+    cout << summary << endl;
+    return 0;
+
+    string a, b;
     cin >> a >> b;
 
     string result;
+
     natural_add_ap(result, a, b);
+    cout << result << endl;
+
+    natural_mul_ap(result, a, b);
 
     cout << result << endl;
 
